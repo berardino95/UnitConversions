a13820f17f6bed6dee7b5bcc2d7ed9cfff9a42e4
Berardino Chiarello <berardino95@gmail.com>
2023-04-21 19:13:26 +0200

    Initial Commit

---
+++ UnitConversions/UnitConversionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionsApp.swift
+//  UnitConversions
+//
+//  Created by CHIARELLO Berardino - ADECCO on 21/04/23.
+//
+
+import SwiftUI
+
+@main
+struct UnitConversionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
